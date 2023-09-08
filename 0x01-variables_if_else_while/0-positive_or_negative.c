8606b57da8a9304fb7eb567b4b23c30c4d1d1f1e
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-08 19:30:16 +0200

    n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,15 +7,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("The number is positive");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("The number is zero");
+printf("%d is zero\n", n);
 }
 else
 {
-printf("The number is negative");
+printf("%d is negative\n", n);
 }
 return (0);
 }
