c74ec827f375337d80f82d7e5910edaf464f83a2
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-09 02:25:03 +0200

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - main entry point
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+/**
+*return : 0 (succese)
+*/
+		return (0);
+}
