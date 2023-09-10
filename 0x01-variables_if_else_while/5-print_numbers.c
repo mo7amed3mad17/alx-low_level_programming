1169e9d845e9fb68895977587905d934107cb6bf
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-10 13:28:23 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry Point
+  *Description: numbers
+  *return: 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	printf("%d", a);
+	}
+	printf("\n");
+
+	return (0);
+}
