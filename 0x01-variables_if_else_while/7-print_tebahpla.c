8ba4d8efefc2f48d7bff88cb6106543448f4f5b0
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-10 17:18:37 +0200

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry Point
+  *Description: Reverse
+  *Return: 0
+*/
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
