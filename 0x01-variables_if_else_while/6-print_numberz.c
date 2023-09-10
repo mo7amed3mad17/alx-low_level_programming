698ed23dcd75eaaf2dc32b763d511b94ee7b07c4
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-10 13:49:38 +0200

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Entry Point
+  *Description: numberz
+  *Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+		putchar('\n');
+	return (0);
+}
