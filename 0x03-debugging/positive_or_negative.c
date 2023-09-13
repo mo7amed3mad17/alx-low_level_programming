9743ec425ac5dae7633bcf9a0fa36dd1a54d78d6
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-13 18:10:29 +0200

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *positive_or_negative - function
+ *@i: checked num
+ *Return: 0
+*/
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
