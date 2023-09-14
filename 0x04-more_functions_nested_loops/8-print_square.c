8e211327d33c50ac11a9e1e8d4a9034ab758c100
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-15 01:12:18 +0200

    8-square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  *print_square - The used func
+  *@size: input
+  *Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+				_putchar('\n');
+		}		
+}
