69c4dc5cb856299f59dcb3e516963211f141dfb5
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-15 01:52:29 +0200

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *main - main entry
+  *Description: fizzbuzz
+  *Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3) == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else if ((x % 15) == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		printf("%d ", x);
+	}
+		printf("\n");
+	return (0);
+}
