210854a645ee25c7aa17dcb71bee56aa9fbecd50
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-15 02:15:49 +0200

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,23 @@ int main(void)
 		if ((x % 3) == 0)
 		{
 			printf("Fizz");
-			continue;
 		}
 		else if ((x % 5) == 0)
-		{
-			printf("Buzz");
-			continue;
-		}
-		else if (x == 100)
 		{
 			printf("Buzz");
 		}
 		else if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
-			continue;
 		}
-		printf("%d ", x);
+		else 
+		{
+			printf("%d ", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
 	}
 		printf("\n");
 	return (0);
