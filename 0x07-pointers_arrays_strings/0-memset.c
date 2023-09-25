ed53a7460dfdf6bbd164edb1df16e6b77dbd5dd2
mo7amed3mad17 <memo12317emad@gmail.com>
2023-09-25 15:32:57 +0200

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _memset - The used func
+  * @s: address
+  * @b: constant byte
+  * @n: num of bytes
+  * Return: nothing
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
