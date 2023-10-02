7f7b12c625cf050f43aa663a36480c8923b19acb
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-02 22:53:24 +0200

    names

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 /**
   * main - Entry Point
+  * @argc: num of args
+  * @argv: strings of args
   * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
