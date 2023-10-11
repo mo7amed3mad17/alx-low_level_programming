881d050ddf2db93ba7b5a0846bcf7cae0f1cd94e
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-12 01:19:57 +0200

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
