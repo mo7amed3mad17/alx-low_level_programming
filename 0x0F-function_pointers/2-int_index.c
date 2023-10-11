53f763493e76593c3d60d0eb3ea29eb3e8483bea
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-11 16:36:21 +0200

    compare

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+  * int_index - The func to be built
+  * @array: input
+  * @size: input
+  * @cmp: function pointer
+  * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
