60b2ab09780b6996ae1a2c13c1ee7134317f15c6
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-13 01:40:27 +0200

    all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 #include <stdarg.h>
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 #endif
