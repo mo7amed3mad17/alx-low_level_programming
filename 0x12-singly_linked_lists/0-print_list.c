b3383414086f51a9bcb51e6b40fc992f4109cc28
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-19 20:34:06 +0200

    Print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * print_list - The function to be build
+  * @h: pointer to the wanted list
+  * Return: The num of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+			i++;
+			h = h->next;
+	}
+			return (i);
+}
