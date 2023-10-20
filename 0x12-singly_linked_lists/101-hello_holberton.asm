61f4f0621b1cccd0a473decb6ee4d1651f367236
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-20 04:40:04 +0200

    mod

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,9 +3,9 @@ section		.text
 	global	main
 
 main:
-	mov		edi, msg
+	mov		edi, moh
 	mov		eax, 0
 	call	printf
 
 section		.data
-	msg db 'Hello, Holberton', 0xa, 0
+	moh: db `Hello, Holberton\n`, 0xa, 0
