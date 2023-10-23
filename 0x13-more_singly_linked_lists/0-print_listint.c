e0dfdc532b7a72cbda28108a9d8bd8350c6a83e4
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-23 08:06:36 +0200

    print_listin

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * print_listint - The function to be built
+  * @h: The node to be printed
+  * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
