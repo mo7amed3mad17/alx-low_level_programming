0f8a59660fa7b39cbbe95adc000ca48149cb24be
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-26 15:49:58 +0200

    Binary_to_decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - The function to be built
+ * @b: The binary number
+ * Return: Uint
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int length = strlen(b);
+	unsigned int n = 0;
+
+	if (!b)
+	return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+	}
+
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			n += pow(2, length - 1 - i);
+		}
+
+	}
+	return (n);
+}
