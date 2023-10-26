c077af0363f2340b732391d0dd6494e186650225
mo7amed3mad17 <memo12317emad@gmail.com>
2023-10-26 20:06:08 +0200

    To_binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,6 @@
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-unsigned long int _pow(unsigned int base, unsigned int power);
 void print_binary(unsigned long int n);
 
 #endif
