37fdb7cc50e753491e6fa17aeae3c395d9f01676
mo7amed3mad17 <memo12317emad@gmail.com>
2023-12-07 12:55:18 +0200

    end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+  * add_dnodeint_end - The function to be built
+  * @head: head of the list
+  * @n: data to be added
+  * Return: head of new list
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *ptr;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	ptr = *head;
+
+	if (ptr != NULL)
+	{
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+			ptr->next = new;
+	}
+
+	else
+	{
+		*head = new;
+	}
+	new->prev = ptr;
+	return (ptr);
+}
