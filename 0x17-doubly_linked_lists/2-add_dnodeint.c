7a0ddcceb5e098261fd39d6b20f5c233010cab8d
mo7amed3mad17 <memo12317emad@gmail.com>
2023-12-07 12:00:41 +0200

    add

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+  * add_dnodeint - The function to be built
+  * @head: head of the list
+  * @n: the data to be added
+  * Return: head of new list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *ptr;
+
+	ptr = malloc(sizeof(dlistint_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr->n = n;
+	ptr->next = *head;
+	ptr->prev = NULL;
+
+	*head = ptr;
+
+	return (ptr);
+}
