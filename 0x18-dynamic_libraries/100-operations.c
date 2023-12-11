9f08fef4d17b2233834203248f6b8422329deb9e
mo7amed3mad17 <memo12317emad@gmail.com>
2023-12-11 14:11:23 +0200

    oprand

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+  * add - add numbers
+  * @n1: num1
+  * @n2: num2
+  * Return: int
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+/**
+  * sub - sub numbers
+  * @n1: num1
+  * @n2: num2
+  * Return: int
+ */
+int sub(int n1, int n2)
+{
+	return (n1 - n2);
+}
+/**
+  * mul - mul numbers
+  * @n1: num1
+  * @n2: num2
+  * Return: int
+ */
+int mul(int n1, int n2)
+{
+	return (n1 * n2);
+}
+/**
+  * div - div numbers
+  * @n1: num1
+  * @n2: num2
+  * Return: int
+ */
+int div(int n1, int n2)
+{
+	return (n1 / n2);
+}
+/**
+  * mod - mod numbers
+  * @n1: num1
+  * @n2: num2
+  * Return: int
+ */
+int mod(int n1, int n2)
+{
+	return (n1 % n2);
+}
